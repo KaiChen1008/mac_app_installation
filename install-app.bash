90e200138536725cf25b526ab8e99e73a461c97e
林楷宸 <kaichen@linkaichendeMacBook-Pro.local>
2019-10-19 16:59:13 +0800

    add zsh

--- install-app.bash
+++ install-app.bash
@@ -4,12 +4,12 @@
 sudo -v
 
 # install homwbrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
-    < /dev/null 2> /dev/null ; \
-    brew install caskroom/cask/brew-cask 2> /dev/null
+#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
+#    < /dev/null 2> /dev/null ; \
+#    brew install caskroom/cask/brew-cask 2> /dev/null
 
 # install wget
-brew install wget --with-libressl
+#brew install wget --with-libressl
 
 # move to Download
 cd ~/Download
@@ -36,6 +36,10 @@ wget https://dl.devmate.com/com.macpaw.site.theunarchiver/TheUnarchiver.zip
 unzip TheUnarchiver.zip
 mv The\ Unarchiver /Applications
 
+
+# set HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_AUTO_UPDATE=1
+
 # install chrome
 brew cask install google-chrome
 
@@ -47,6 +51,29 @@ brew cask install typora
 
 # install slack
 brew cask install slack 
+
+# install oh my zsh
+# see for more details
+# https://medium.com/statementdog-engineering/prettify-your-zsh-command-line-prompt-3ca2acc967f
+
+# install item
+brew cask instal iterm2
+# clone iterm color
+
+git clone https://github.com/mbadolato/iTerm2-Color-Schemes
+
+
+# 先執行這行，才能用 homebrew 安裝字型。曾經執行過的人可以跳過這個指令
+brew tap homebrew/cask-fonts
+
+# download font 
+brew cask install font-sourcecodepro-nerd-font
+
+# install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install oh my zsh theme
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 
 echo " you have to install \n line\n xcode\n cahtemate for FB\n Debit\n & Credit\n pages\n numbers\n keynote\n from app store"
